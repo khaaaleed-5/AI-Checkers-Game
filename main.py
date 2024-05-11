3fab061050d8041f6ef6fb1849f56abc006f4186
khaaaleed-5 <khaled.20210298@gmail.com>
2024-05-11 05:01:47 +0300

    updated

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from model import Player
 game = Game()
 genetic_algorithm = GeneticAlgorithm(population_size=50,mutation_rate=0.76,nn=Player,game=game)
 
-best_player = genetic_algorithm.train(100,thresh_fitness=6)
+best_player = genetic_algorithm.train(epochs=100,thresh_fitness=6)
